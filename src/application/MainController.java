10fb6a5633d331846bcde34dc25237ec1bec7093
timdpr <qotimsa@live.co.uk>
2017-07-20 16:33:28 +0100

    Formatting

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -15,55 +15,55 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MainController implements Initializable {
-	
-	@FXML
-	private Button getSalesReport;
-	@FXML
-	private Button getStockSheet;
-	@FXML
-	private Label errorLabel;
-	@FXML
-	private ImageView imageView;
-	
-	private File salesReport;
-	private File stockSheet;
-	
-	@FXML
-	protected void update(ActionEvent event) {
-		try {
-			salesReport.exists();
-			stockSheet.exists();
-		} catch (NullPointerException e) {
-			errorLabel.setText("Error: Make sure both spreadsheets are selected");
-		}
-		try {
-		    StockSheetUpdater updater = new StockSheetUpdater(salesReport, stockSheet, errorLabel);
-		    updater.update();
-		} catch (NullPointerException e) {
-			System.out.println("End of reportSheet rows");
-		}
-	}
-	
-	@FXML
-	protected void getSalesReport(ActionEvent event) {
-	    this.salesReport = getFile();
-	    getSalesReport.setText("Selected");
-	}
-	
-	@FXML
-	protected void getStockSheet(ActionEvent event) {
-		this.stockSheet = getFile();
-		getStockSheet.setText("Selected");
-	}
-	
-	public File getFile() {
-		FileChooser chooser = new FileChooser();
-		File file = chooser.showOpenDialog(new Stage());
-		return file;
-	}
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		imageView.setImage(new Image ("/tartarus-press.jpg"));
-	}
+
+    @FXML
+    private Button getSalesReport;
+    @FXML
+    private Button getStockSheet;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private ImageView imageView;
+
+    private File salesReport;
+    private File stockSheet;
+
+    @FXML
+    protected void update(ActionEvent event) {
+        try {
+            salesReport.exists();
+            stockSheet.exists();
+        } catch (NullPointerException e) {
+            errorLabel.setText("Error: Make sure both spreadsheets are selected");
+        }
+        try {
+            StockSheetUpdater updater = new StockSheetUpdater(salesReport, stockSheet, errorLabel);
+            updater.update();
+        } catch (NullPointerException e) {
+            System.out.println("End of reportSheet rows");
+        }
+    }
+
+    @FXML
+    protected void getSalesReport(ActionEvent event) {
+        this.salesReport = getFile();
+        getSalesReport.setText("Selected");
+    }
+
+    @FXML
+    protected void getStockSheet(ActionEvent event) {
+        this.stockSheet = getFile();
+        getStockSheet.setText("Selected");
+    }
+
+    public File getFile() {
+        FileChooser chooser = new FileChooser();
+        File file = chooser.showOpenDialog(new Stage());
+        return file;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        imageView.setImage(new Image ("/tartarus-press.jpg"));
+    }
 }
